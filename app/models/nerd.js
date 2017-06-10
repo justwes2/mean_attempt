f4b0ba9ed4fa53155940b738813bb4f560ffb386
Wes <wes.coffay@gmail.com>
2017-06-09 20:58:08 -0400

    build out app structure

--- app/models/nerd.js
+++ app/models/nerd.js
@@ -6,3 +6,6 @@ const mongoose = require('mongoose');
 module.exports =  mongoose.model('Nerd', {
   name : {type : String, default: ''}
 })
+
+
+//all other backend Node stuff goes in app folder as well
