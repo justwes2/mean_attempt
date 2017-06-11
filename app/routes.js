d97a4abec190418040eba5848fb2e4b4a6b9b13c
Wes <wes.coffay@gmail.com>
2017-06-11 00:07:53 -0400

    built backend arch

---
+++ app/routes.js
@@ -0,0 +1,27 @@
+//pull in model
+const Nerd = require('./models/nerd')
+
+  module.exports = function(app) {
+    //server routes (api calls, auth routes)
+
+    //sample api route
+    app.get('/api/nerds', function(req, res) {
+      //use mongoose to get all nerds in db
+      Nerd.fund(function(err, nerds) {
+        //if error send error, nothing else will exectute
+        if (err)
+          res.send(err)
+        res.json(nerds)
+
+      })
+    })
+    //create route goes here (app.post)
+    //delete route goes here (app.delete)
+
+    //frontend routes
+    //route to handle angular requests
+    app.get('*', function(req, res) {
+      res.sendfile('.public/views/index.html') //loads index file
+    })
+    
+  }
